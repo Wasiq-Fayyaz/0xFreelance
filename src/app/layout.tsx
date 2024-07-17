c41a9e955d9df1aebcc8ad34e32e548d17d3596b
Wasiq-Fayyaz <wasiqfayaz7@gmail.com>
2024-07-17 16:41:34 +0500

    Finalized

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ import { ToastContainer } from 'react-toastify'
 
 const inter = Inter({ subsets: ['latin'] })
 const exo = Exo({
-  weight: ['400', '700'], // Specify the font weights you want to use
+  weight: ['100','200','300','400','500','600','700','800'], // Specify the font weights you want to use
   subsets: ['latin'], // Specify the subsets you need
   display: "swap",
 });
